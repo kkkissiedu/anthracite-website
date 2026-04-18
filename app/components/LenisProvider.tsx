38b96535210a82338aa515aac400538a4b4d3dd8
Heisenberg <kissiedukwabena4@gmail.com>
2026-04-18 18:17:33 -0100

    fix: reduce Lenis scroll lag

--- app/components/LenisProvider.tsx
+++ app/components/LenisProvider.tsx
@@ -12,9 +12,10 @@ export default function LenisProvider({
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.12,
+      duration: 0.8,
       smoothWheel: true,
+      wheelMultiplier: 1.2,
     });
 
     lenisRef.current = lenis;
