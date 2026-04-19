3cabc3501af895d5e2b6edd5eb05350b02ee08ba
Heisenberg <kissiedukwabena4@gmail.com>
2026-04-19 18:37:14 -0100

    changes

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "x9ubqazh",
+    dataset: "production",
+  },
+  deployment: {
+    appId: "pm090dg5n39liwyc1vzi9yxk",
+  },
+});
